057f15d442716acc24f26d14a013e3d477e1ad5b
Felipe Duval <fduval@ic.uff.br>
2013-06-07 08:19:07 -0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Restserveruff::Application.config.secret_token = '0d6440c253f38549b5e764f9b5e6a12e94688ae4134f42da72c51178d9117673657427968cd10a2a2db8a54ed3deea0d576c05de439a14bd6391cfb6910572ab'
