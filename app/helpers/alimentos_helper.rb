b36fed8e7e8c97e955fd3b7a9b8ab19b3b4ae101
Felipe Duval <fduval@ic.uff.br>
2013-07-05 08:08:49 -0300

    commit 5

--- app/helpers/alimentos_helper.rb
+++
@@ -1,2 +0,0 @@
-module AlimentosHelper
-end
--- app/models/alimento.rb
+++
@@ -1,7 +0,0 @@
-class Alimento < ActiveRecord::Base
-  attr_accessible :nome, :tipo, :preco
-  
-  has_many :precos
-  has_many :estabelecimentos, through: :precos
-  has_one :preco, :through => :estabelecimento
-end
